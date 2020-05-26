c4595bc7736c437350447385577da5178b928104
la-ki <layarkostic94@gmail.com>
2020-05-26 16:47:17 +0200

    Created project and configured Express server

---
+++ src/controllers/RegisterController.js
@@ -0,0 +1,34 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+    async store(req, res) {
+        try {
+
+            const { firstName, lastName, password, email } = req.body;
+
+            const existentUser = await User.findOne({ email });
+
+            if (!existentUser) {
+                const hashedPassword = await bcrypt.hash(password, 10);
+                const user = await User.create({
+                    firstName: firstName,
+                    lastName: lastName,
+                    password: hashedPassword,
+                    email: email
+                });
+                return res.json(user);
+            }
+
+            return res.status(400).json({
+                message: 'email/user already exist! do you want to ligin instead?'
+            })
+
+        } catch (error) {
+
+
+            throw Error(`Error while registering a new user : ${error}`);
+
+        }
+    }
+}
---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const RegisterController = require('./controllers/RegisterController');
+
+const PORT = process.env.PORT || 8000;
+
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello From Nodemon");
+});
+
+app.post('/register', RegisterController.store);
+
+try {
+    mongoose.connect(process.env.MONGO_DB_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log('MongoDB Connected');
+} catch (error) {
+    console.log('Error connection to MongoDB');
+}
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+});
