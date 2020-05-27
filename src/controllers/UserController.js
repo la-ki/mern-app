0847e371a4f4ff1a558fc0db5efd82778a3b0c4a
la-ki <layarkostic94@gmail.com>
2020-05-27 15:42:39 +0200

    Added API Events

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,13 +3,9 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
     async createUser (req, res) {
-        
         try {
-
             const { firstName, lastName, password, email } = req.body;
-
             const existentUser = await User.findOne({ email });
-
             if (!existentUser) {
                 const hashedPassword = await bcrypt.hash(password, 10);
                 const user = await User.create({
@@ -20,38 +16,22 @@ module.exports = {
                 });
                 return res.json(user);
             }
-
             return res.status(400).json({
                 message: 'email/user already exist! do you want to ligin instead?'
             })
-
         } catch (error) {
-
-
             throw Error(`Error while registering a new user : ${error}`);
-
         }
     },
-
     async getUserById (req, res) {
-
         const { userId } = req.params;
-
         try {
-
             const user = await User.findById(userId);
-
             return res.json(user);
-
         } catch (error) {
-
             return res.status(400).json({
-
                 message: 'User ID does not exist, do you want to register instead?'
-
-            })
-
+            });
         }
     }
-
 }
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const multer = require('multer');
 
 const UserController = require('./controllers/UserController');
 const EventController = require('./controllers/EventController');
+const DashboardController = require('./controllers/DashboardController');
+const LoginController = require('./controllers/LoginController');
 const uploadConfig = require('./config/upload');
 
 const routes = express.Router();
@@ -14,17 +16,32 @@ routes.get('/status', (req, res) => {
     res.send({status: 200});
 });
 
+// TODO Subscribe Controller
+// TODO Approval Controller
+// TODO Rejection Controller
+// TODO Login Controller
+
+// LOGIN
+
+routes.post('/login', LoginController.store);
+
+// DASHBOARD
+
+routes.get('/dashboard/:sport', DashboardController.getAllEvents);
+routes.get('/dashboard', DashboardController.getAllEvents);
+routes.get('/event/:eventId', DashboardController.getEventById);
+
 // EVENT
 
-routes.get('/event/:eventId', EventController.getEventById);
+
 
 routes.post('/event', upload.single("thumbnail"), EventController.createEvent);
+routes.delete('/event/:eventId', EventController.deleteEvent);
 
 
 // USER
 
 routes.post('/user/register', UserController.createUser);
-
 routes.get('/user/:userId', UserController.getUserById);
 
 module.exports = routes;
